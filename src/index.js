92a623f109f02aa6d067746c9a374e661cba3aae
chendch <chendch@yonyou.com>
2018-11-27 18:37:10 +0800

    Chern1992

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Route} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+require('./index.css');
 
 ReactDOM.render(<Provider>
     <BrowserRouter>
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,17 +1,17 @@
 import React, { Component, Fragment } from 'react';
-import {NForm, NButton, NIcon, NInput, NCheckbox, NHeader} from 'shared/base/index';
+import {NForm, NButton, NIcon, NInput, NCheckbox} from 'shared/base/index';
 import ReactDOM from 'react-dom';
-import Page from 'shared/Page';
 import './index.less';
 
 const NFormItem = NForm.NFormItem;
 
-class Login extends Page{
+class Login extends Component{
     constructor(props) {
         super(props);
-        // this.state = {
-        //     // sum: 111
-        // }
+        this.state = {
+            userName: '',
+            password: ''
+        }
     }
 
     componentDidMount() {
@@ -27,6 +27,7 @@ class Login extends Page{
         this.props.form.validateFields((err, values) => {
           if (!err) {
             console.log('Received values of form: ', values);
+            
           }
         });
     }
@@ -35,9 +36,43 @@ class Login extends Page{
         const { getFieldDecorator } = this.props.form;
         
         return (
-            <div className="login-bg-container">
-                555
-                {/* {this.modal.createModal('ddd')} */}
+            <div className="login-box-container">
+                <img src={require("../../assets/images/login/normal.png")}/>
+                <div className="auth-modal-box">
+                    <h2>登录</h2>
+                    <NForm onSubmit={this.handleSubmit} className="login-form">
+                        <NFormItem>
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <NInput prefix={<NIcon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                            )}
+                        </NFormItem>
+                        <NFormItem>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <NInput prefix={<NIcon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                            )}
+                        </NFormItem>
+
+                        <NFormItem>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(
+                                <NCheckbox>记住密码</NCheckbox>
+                            )}
+                            <a className="login-form-forgot" href="">忘记密码</a>
+                            <NButton type="primary" htmlType="submit" className="login-form-button">
+                                登陆
+                            </NButton>
+                            Or <a href="">注册</a>
+                        </NFormItem>
+                    </NForm>
+                    
+                    
+                </div>
             </div>
         )
     }
